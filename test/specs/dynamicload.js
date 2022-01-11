9b259e6927f66d2f4bb58533c81426271fe2149b
Ned Wilbur <Ned612@Gmail.com>
2022-01-10 21:17:50 -0500

    Added 'Dynamically Loaded Page Elements - 2'

--- test/specs/dynamicload.js
+++ test/specs/dynamicload.js
@@ -1,4 +1,3 @@
-import dynamicloadView from '../views/dynamicload.view';
 import DynamicLoadView from '../views/dynamicload.view';
 
 describe('Dynamically Loaded Page Elements - 1', () => {
@@ -12,6 +11,22 @@ describe('Dynamically Loaded Page Elements - 1', () => {
 
     it('Element visible after load', async () => {
         await expect(DynamicLoadView.FinishText).not.toBeDisplayed();
+        await DynamicLoadView.StartButton.click();
+        await expect(DynamicLoadView.FinishText).toBeDisplayed();
+    });
+});
+
+describe('Dynamically Loaded Page Elements - 2', () => {
+    beforeEach(async () => {
+        await DynamicLoadView.navTo2();
+    });
+
+    it('header', async () => {
+        await expect(DynamicLoadView.Header).toHaveText(DynamicLoadView.Title);
+    });
+
+    it('Element visible after load', async () => {
+        await expect(DynamicLoadView.FinishText).not.toExist();
         await DynamicLoadView.StartButton.click();
         await expect(DynamicLoadView.FinishText).toBeDisplayed();
     });
